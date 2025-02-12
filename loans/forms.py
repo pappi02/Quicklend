82c85524fe7218db8fb303273673de9869385dd3
pappi02 <127497278+pappi02@users.noreply.github.com>
2025-02-11 23:11:18 -0800

    Update forms.py

--- loans/forms.py
+++ loans/forms.py
@@ -1,15 +1,11 @@
 from django import forms
-from .models import Borrower, Loan, Collateral, Payment
+from .models import Borrower, Loan, Collateral, Payment, Guarantor  # Add Guarantor model
 
 
 class LoanForm(forms.ModelForm):
     class Meta:
         model = Loan
         fields = ['amount', 'interest_rate', 'repayment_period_weeks', 'start_date', 'end_date']
-
-
-
-
 
 
 class CollateralForm(forms.ModelForm):
@@ -20,12 +16,20 @@ class CollateralForm(forms.ModelForm):
             'status': forms.Select(choices=Collateral.COLLATERAL_STATUS_CHOICES),
         }
 
+
 class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = ['amount', 'date', 'payment_type']
 
+
 class BorrowerForm(forms.ModelForm):
     class Meta:
         model = Borrower
-        fields = ['name', 'phone', 'email', 'business_type']
+        fields = ['name', 'phone', 'id_number', 'email', 'business_type']
+
+
+class GuarantorForm(forms.ModelForm):
+    class Meta:
+        model = Guarantor
+        fields = ['names', 'phonenumber', 'national_id', 'emails']
